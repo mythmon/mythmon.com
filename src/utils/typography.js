60f22ee92fb9b175c95ddee92789eefbb3456615
Mike Cooper <mythmon@gmail.com>
2019-03-02 21:44:59 -0800

    Actually include typography config

---
+++ src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typography from "typography";
+import parnassusTheme from "typography-theme-parnassus";
+
+parnassusTheme.overrideStyles = () => ({});
+
+const typography = new Typography(parnassusTheme);
+
+export default typography;
