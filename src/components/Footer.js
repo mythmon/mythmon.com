45c7ab6135738df29a1cc2fcb8c58409f6211420
Mike Cooper <mythmon@gmail.com>
2019-03-08 12:27:53 -0800

    Tweak sizing more

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer({ siteTitle }) {
         Source on <a href="https://github.com/mythmon/mythmon.com">Github</a>.
       </p>
       <iframe
-        style={{ width: "100%", maxWidth: 800, height: 60 }}
+        style={{ width: "100%", maxWidth: 960, height: 60 }}
         title="webring-nav"
         src="https://abelian.now.sh/embed/"
         frameBorder="no"
